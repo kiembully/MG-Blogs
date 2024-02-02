b45e1376c06c6892be4a52c7b999a02a16d00ae2
kiembully <kimklintonvale@gmail.com>
2024-02-02 11:21:24 +0800

    add components folder

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,9 @@
+import NavMenu from "../components/navmenu";
+
 const MainPage = () => {
   return (
     <div className="absolute top-0 left-0 h-screen w-screen bg-green-500">
+      <NavMenu />
       asdsadsada
     </div>
   );
