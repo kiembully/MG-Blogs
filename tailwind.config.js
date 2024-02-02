6bf0e89ccaa0ec1f0334bebea2c6930d81fde662
Tristan John P. Girao <tristanjohn@mashupgarage.com>
2024-02-02 15:30:41 +0800

    [Feat] fix ui of login and signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      'feed': '#EBF0F4',
-      'white': '#ffffff',
-      'neutral-100': '#ECF1F3',
-      'neutral-200': '#D2D2D3',
-      'neutral-800': '#111215',
-      'orange': '#C8512C',
-      'primary-500': '#6366F1'
+    extend: {
+      colors: {
+        feed: "#EBF0F4",
+        white: "#ffffff",
+        "neutral-100": "#ECF1F3",
+        "neutral-200": "#D2D2D3",
+        "neutral-800": "#111215",
+        orange: "#C8512C",
+        "primary-500": "#6366F1",
+      },
+      spacing: {
+        13: "3.25rem",
+        15: "3.75rem",
+        128: "32rem",
+        144: "36rem",
+        sm: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "24px",
+      },
     },
-    spacing: {
-      '13': '3.25rem',
-      '15': '3.75rem',
-      '128': '32rem',
-      '144': '36rem',
-      sm: '8px',
-      md: '12px',
-      lg: '16px',
-      xl: '24px',
-    },
-    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
