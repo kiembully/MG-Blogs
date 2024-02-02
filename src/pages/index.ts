ac3a3bb600fe8f21e557abe56561843bf3692b26
Tristan John P. Girao <tristanjohn@mashupgarage.com>
2024-02-02 10:50:55 +0800

    [Feat] Adds folder structure and routing

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+// export * from "./account";
+export { default as CreatePost } from "./CreatePost";
+export { default as EditPost } from "./EditPost";
+export { default as Login } from "./Login";
+export { default as MainPage } from "./MainPage";
+export { default as Signup } from "./Signup";
