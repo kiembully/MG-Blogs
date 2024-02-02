9210076bb47cda6a7869ada762f7132e2979616e
Tristan John P. Girao <tristanjohn@mashupgarage.com>
2024-02-02 14:22:51 +0800

    feat: Applies states in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,35 +1,69 @@
+import { useState } from "react";
 import { PageLayout } from "../components";
 
-const Login = () => {
+type LoginTypes = {
+  username: string;
+  password: string;
+};
+
+const Login: React.FC = () => {
+  const [credentials, setCredentials] = useState<LoginTypes>({
+    username: "",
+    password: "",
+  });
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setCredentials((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const onSubmit = () => {
+    console.log({ credentials });
+  };
+
   return (
     <PageLayout>
-      <div className="relative h-3/4 w-3/5 flex flex-row">
-        <div className="relative h-full w-1/2 bg-black/75"></div>
+      <div className="relative h-full w-full flex flex-row">
+        <img
+          src="/assets/mg-background.png"
+          alt="mg-background"
+          className="relative h-full w-1/2 aspect-auto"
+        />
         <form className="relative h-full w-1/2 bg-white flex items-center justify-center">
           <span className="absolute top-6 right-8 text-sm">
             New to Mashup Garage Blogs?{" "}
-            <a href="#" className="text-[#6366F1]">
+            <a
+              href="/signup"
+              className="underline text-[#6366F1] underline-offset-2"
+            >
               Sign up
             </a>
           </span>
-          <div className="relative h-80 w-72 flex flex-col item-start justify-start">
+          <div className="relative w-72 flex flex-col item-start justify-start mb-28">
             <p className="text-xl">Login</p>
             <p className="text-sm mt-6">Username</p>
             <input
               type="text"
+              value={credentials.username}
+              name="username"
+              onChange={onChange}
               className="h-10 w-full mt-2 bg-[#ECF1F3] outline-none text-sm pl-4 text-black/75"
             />
             <p className="text-sm mt-6">Password</p>
             <input
               type="password"
+              value={credentials.password}
+              name="password"
+              onChange={onChange}
               className="h-10 w-full mt-2 bg-[#ECF1F3] outline-none text-sm pl-4 text-black/75"
             />
             <a href="#" className="text-xs text-[#6366F1]">
               Forgot your password?
             </a>
             <button
-              type="submit"
-              className="absolute bottom-0 h-10 w-full bg-[#6366F1] rounded text-white"
+              type="button"
+              onClick={() => onSubmit()}
+              className="relative mt-12 h-10 w-full bg-[#6366F1] rounded text-white"
             >
               Login
             </button>
