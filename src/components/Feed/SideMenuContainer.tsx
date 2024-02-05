971b0f78b20349162d68e048eb3df2de282d5646
kiembully <kimklintonvale@gmail.com>
2024-02-05 15:38:26 +0800

    connecting routes to each page

--- src/components/Feed/SideMenuContainer.tsx
+++ src/components/Feed/SideMenuContainer.tsx
@@ -3,17 +3,25 @@ import { Options, Languages, Footer } from './index'
 import Tags from '../Tags'
 import Button from '../button'
 import ProfileCard from '../Profile/ProfileCard'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {
-  variant?: 'feed' | 'profile' // profile states
+  variant?: 'feed' | 'profile'
+  isUserActive?: boolean // profile states
 }
 
-const SideMenuContainer: FC<Props> = ({ variant }) => {
+const SideMenuContainer: FC<Props> = ({ variant, isUserActive }) => {
+  const navigate = useNavigate()
+
   return (
     <>
       {variant === 'feed' ? (
         <>
-          <Button fullWidth>Create Post</Button>
+          {isUserActive && (
+            <Button onClick={() => navigate('/post/1/create-post')} fullWidth>
+              Create Post
+            </Button>
+          )}
           <Tags />
           <Options />
           <Languages />
