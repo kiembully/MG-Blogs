8502c0abe105fe5c28ea3dbd56021db37237f153
Tristan John P. Girao <tristanjohn@mashupgarage.com>
2024-02-05 13:13:04 +0800

    [Update] Changes snake case to camel case in interfaces.ts

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -13,13 +13,13 @@ export interface LoginCredentials {
 export interface User {
   username: string
   name: string
-  created_at: Date
-  updated_at: Date
+  createdAt: Date
+  updatedAt: Date
   email: string
-  encrypted_password: string
-  reset_password_token: string
-  reset_password_sent_at: Date
-  remember_created_at: Date
+  encryptedPassword: string
+  resetPasswordToken: string
+  resetPasswordSentAt: Date
+  rememberCreatedAt: Date
   profilePicture?: string | Blob
   posts?: Post[]
   comments?: Comment[]
@@ -29,8 +29,8 @@ export interface Post {
   creatorID: BigInt
   title: string
   body: string
-  created_at: Date
-  updated_at: Date
+  createdAt: Date
+  updatedAt: Date
   tags: Tags[]
   voteCounts: VoteCount
   image?: string | Blob
@@ -46,14 +46,14 @@ export interface Comment {
   creatorID: BigInt
   postID: BigInt
   message: string
-  created_at: Date
-  updated_at: Date
+  createdAt: Date
+  updatedAt: Date
   voteCounts: VoteCount
   replies: Comment[]
 }
 
 export interface VoteCount {
-  reference_id: BigInt //id of the post of comment
+  referenceId: BigInt //id of the post of comment
   upVotes: string[] //array of user id's that has up vote
   downVotes: string[] //array of user id's that has down vote
   //will use hashmap to check if a user has upvoted or downvoted the post or comment to render correct icon. It will have O(1) notation.
