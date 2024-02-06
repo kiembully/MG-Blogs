f05935fe86ba5184a8d2528dfdba1df7bdb85b3f
Tristan John P. Girao <tristanjohn@mashupgarage.com>
2024-02-06 22:21:42 +0800

    [Feat] added profile dropdown modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './layouts'
 export * from './Feed'
 export { default as AuthenticationPagesLayout } from './AuthenticationPagesLayout'
+export { default as ProfileOptionModal } from './ProfileOptionModal'
