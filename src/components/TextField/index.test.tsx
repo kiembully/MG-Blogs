110d655da229377597eda45f66ce03dace8afe49
xinjo21 <elrazinmjo@gmail.com>
2024-02-07 16:03:51 +0800

    Initial commit for unit test

---
+++ src/components/TextField/index.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import TextField from '.'
+
+describe('Input Field', () => {
+  it('should display input with label', () => {
+    render(<TextField type='text' label='Username' placeholder='Enter your username' />)
+    expect(screen.getByLabelText('label')).toHaveTextContent('Username')
+    expect(screen.getByPlaceholderText('Enter your username')).toBeInTheDocument()
+    expect(screen.getByLabelText('input-field')).toBeEnabled()
+  })
+
+  it('should handles onChange event correctly', () => {
+    const mockOnChange = jest.fn()
+    render(<TextField type='password' label='Username' name='Username' value='' onChange={mockOnChange} />)
+
+    fireEvent.change(screen.getByLabelText('input-field'), { target: { value: 'username-here' } })
+    expect(mockOnChange).toHaveBeenCalledWith(expect.any(Object))
+  })
+})
