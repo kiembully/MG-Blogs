04c4811328fc54808eacde711f98e692e248636a
xinjo21 <elrazinmjo@gmail.com>
2024-02-08 12:01:49 +0800

    More test

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,22 +1,22 @@
 import { jwtDecode } from 'jwt-decode'
 
 export const checkTokenExpiration = (token: string) => {
-  if (token) {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const decodedToken: any = jwtDecode(token)
-      const isTokenExpired = decodedToken.exp < Date.now() / 1000
-      if (isTokenExpired) {
-        return true
-      } else {
-        return false
-      }
-    } catch (error) {
-      console.error('Error decoding the token:', error)
-      return true
-    }
-  } else {
+  if (!token) {
     console.log('Token not found.')
     return true
   }
+
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const decodedToken: any = jwtDecode(token)
+    const isTokenExpired = decodedToken.exp < Date.now() / 1000
+
+    if (!isTokenExpired) return false
+
+    return true
+  } catch (error) {
+    console.error('Error decoding the token:', error)
+    return true
+  }
 }
+
